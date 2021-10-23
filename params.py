59ba82fb6d6c26a8beb34834542b74917c960e56
Archi <74546582+Archimonde666@users.noreply.github.com>
2021-10-23 10:18:55 +0200

    Delete params.py

--- params.py
+++
@@ -1,7 +0,0 @@
-leaderboard_channel_id = 804443429957664808
-upload_channel_id = 804443389641752597
-admin_role_id = 650631484637708311
-big_decision_admin_count = 1
-elo_bot_voicechat_id = 650619899017953296
-GUILD_NAME = 'BotTest'
-admin_channel_id = 796438422347710474
